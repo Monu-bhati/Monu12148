3bb2bf102939bd54f92ab6ee91c0025ec2376d7a
lenovo <lenovo@lenovo-PC>
2024-07-31 16:09:55 +0530

    commit

---
+++ Jenkins/src/Demo1.java
@@ -0,0 +1,15 @@
+import java.sql.Date;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+	
+		int PostId=101;
+		String PostName="JWT";
+		String Description="JWT is json";
+		Date dateofposting;
+		System.out.println(PostId+" "+PostName+" "+Description);
+
+	}
+
+}
